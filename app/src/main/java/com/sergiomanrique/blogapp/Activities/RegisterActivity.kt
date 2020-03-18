cdfe03c0c34504077f21be5355350f970d1d2105
Sergio Manrique <sergiman94@gmail.com>
2020-03-18 17:11:09 -0500

    First commit

---
+++ app/src/main/java/com/sergiomanrique/blogapp/Activities/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.sergiomanrique.blogapp.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sergiomanrique.blogapp.R
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
